b4de59396844225f13227818c51abe7e06c2613e
Prajakta Patil <prajaktap816@gmail.com>
2026-04-07 20:15:52 +0530

    remaining

--- frontend/learning/src/App.jsx
+++ frontend/learning/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import './App.css';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import Home from './pages/home/home';
+import Header from './components/header/header';
 const App = () => {
-  return <div> App </div>; 
-}
+  return <> 
+  <BrowserRouter>
+  <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
+  </>;
+};
 
 export default App;
